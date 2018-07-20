04a7f07a0b350d88738e3efd915785d89f1d057a
MelisaGumus <melisa.gumus@mail.utoronto.ca>
2018-07-20 15:41:25 -0400

    ratios on BC_BiC_AAC and BC_AAC

--- Current_Ratios/currents_on_BC_AAC_BiC.m
+++ Current_Ratios/currents_on_BC_AAC_BiC.m
@@ -104,8 +104,115 @@ h = fullfile('/home','melisagumus','Documents', ...
     'mytrace_3505_syns.dat';...
     'mytrace_3615_syns.dat'...
     });
+%% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-%% 
+% Load Data 
+clear all
+clc 
+ 
+g = fullfile('/Users','macklabadmin','Documents', ...
+    'other','pvbasket',{...
+    'pvbasket_332810_1000';...
+    'pvbasket_333500_1000';...
+    'pvbasket_333776_1000';...
+    'pvbasket_334466_1000';...
+    'pvbasket_335018_1000';...
+    'pvbasket_335432_1000';...
+    'pvbasket_335846_1000';...
+    'pvbasket_336260_1000';...
+    'pvbasket_336536_1000';...
+    'pvbasket_336674_1000';...
+    'pvbasket_337088_1000';...
+    'pvbasket_337364_1000';...
+    'pvbasket_337640_1000';...
+    'pvbasket_338192_1000'...
+    },{...
+    'mytrace_332810_syns.dat';...
+    'mytrace_333500_syns.dat';...
+    'mytrace_333776_syns.dat';...
+    'mytrace_334466_syns.dat';...
+    'mytrace_335018_syns.dat';...
+    'mytrace_335432_syns.dat';...
+    'mytrace_335846_syns.dat';...
+    'mytrace_336260_syns.dat';...
+    'mytrace_336536_syns.dat';...
+    'mytrace_336674_syns.dat';...
+    'mytrace_337088_syns.dat';...
+    'mytrace_337364_syns.dat';...
+    'mytrace_337640_syns.dat';...
+    'mytrace_338192_syns.dat'...
+    });
+
+f = fullfile('/Users','macklabadmin','Documents', ...
+    'other','AAC',{...
+    'AAC_0_1000';...
+    'AAC_36_1000';...
+    'AAC_180_1000';...
+    'AAC_288_1000';...
+    'AAC_360_1000';...
+    'AAC_468_1000';...
+    'AAC_576_1000';...
+    'AAC_720_1000';...
+    'AAC_828_1000';...
+    'AAC_900_1000';...
+    'AAC_1008_1000';...
+    'AAC_1152_1000';...
+    'AAC_1224_1000';...
+    'AAC_1332_1000';...
+    'AAC_1404_1000'...
+    },{...
+    'mytrace_0_syns.dat';...
+    'mytrace_36_syns.dat';...
+    'mytrace_180_syns.dat';...
+    'mytrace_288_syns.dat';...
+    'mytrace_360_syns.dat';...
+    'mytrace_468_syns.dat';...
+    'mytrace_576_syns.dat';...
+    'mytrace_720_syns.dat';...
+    'mytrace_828_syns.dat';...
+    'mytrace_900_syns.dat';...
+    'mytrace_1008_syns.dat';...
+    'mytrace_1152_syns.dat';...
+    'mytrace_1224_syns.dat';...
+    'mytrace_1332_syns.dat';...
+    'mytrace_1404_syns.dat'...
+    });
+
+h = fullfile('/Users','macklabadmin','Documents', ...
+    'other','BiC',{...
+    'BiC_1470_1000';...
+    'BiC_1580_1000';...
+    'BiC_1635_1000';...
+    'BiC_1855_1000';...
+    'BiC_2020_1000';...
+    'BiC_2130_1000';...
+    'BiC_2350_1000';...
+    'BiC_2460_1000';...
+    'BiC_2570_1000';...
+    'BiC_2900_1000';...
+    'BiC_3010_1000';...
+    'BiC_3175_1000';...
+    'BiC_3340_1000';...
+    'BiC_3505_1000';...
+    'BiC_3615_1000'...
+    },{...
+    'mytrace_1470_syns.dat';...
+    'mytrace_1580_syns.dat';...
+    'mytrace_1635_syns.dat';...
+    'mytrace_1855_syns.dat';...
+    'mytrace_2020_syns.dat';...
+    'mytrace_2130_syns.dat';...
+    'mytrace_2350_syns.dat';...
+    'mytrace_2460_syns.dat';...
+    'mytrace_2570_syns.dat';...
+    'mytrace_2900_syns.dat';...
+    'mytrace_3010_syns.dat';...
+    'mytrace_3175_syns.dat';...
+    'mytrace_3340_syns.dat';...
+    'mytrace_3505_syns.dat';...
+    'mytrace_3615_syns.dat'...
+    });
+%% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %cd C:\Users\Melisa\Documents\MATLAB\CA1_SimTracker\pyr\
 %files = dir('pyr*1000\mytrace*syns.dat');
 %pyr_names = f.names;
@@ -141,7 +248,8 @@ end
 dataBiC = mat2cell(alldataBiC, 40000, ...
     [12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12]);
 
-%% Creates a big table consists of inputs from BiC PYR and BC in order
+%% Creates a big table consists of inputs from BiC PYR and BC in order %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
 
 M_on_BC = [];
 current_BC_on_BC = [];
@@ -194,7 +302,8 @@ end
 allcellsBC = mat2cell(M_on_BC, 40000, ...
     [3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3]); 
 
-%%
+%% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
 M = [];
 current_BC_on_AAC = [];
 current_PYR_on_AAC = [];
@@ -246,7 +355,8 @@ end
 allcellsAAC = mat2cell(M, 40000, ...
     [3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3]); 
 
-%%
+%% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
 M_BiC = [];
 current_BC_on_BiC = [];
 current_PYR_on_BiC = [];
@@ -427,7 +537,7 @@ title('Mean Peak EPSC onto AAC','FontSize',15,'FontWeight','bold')
 
 %% EPSCs from PYR on BiC
 
-EPSC_on_BiC| = [];
+EPSC_on_BiC = [];
 epsc_on_BiC = [];
 for i = 1:1:15 % number of PYR cells
     pks_epsc = allcellsBiC{i}(:,2);
@@ -544,6 +654,7 @@ ylabel('Mean Peak IPSC from BiC','FontSize',13,'FontWeight','bold');
 hold on;
 errorbar(x,IPSC_BiC_on_BiC_mean,IPSC_BiC_on_BiC_std,'b','LineStyle','none')
 title('Mean Peak IPSC from BiC onto BiC','FontSize',15,'FontWeight','bold')
+
 %% IPSCs only from BC on BC
 
 IPSC_BC_on_BC = [];
@@ -604,6 +715,7 @@ ylabel('Mean Peak IPSC from BC','FontSize',13,'FontWeight','bold');
 hold on;
 errorbar(x,IPSC_BC_on_AAC_mean,IPSC_BC_on_AAC_std,'b','LineStyle','none')
 title('Mean Peak IPSC from BC onto AAC','FontSize',15,'FontWeight','bold')
+
 %% IPSCs only from BC on BiC
 
 IPSC_BC_on_BiC = [];
@@ -634,20 +746,44 @@ hold on;
 errorbar(x,IPSC_BC_on_BiC_mean,IPSC_BC_on_BiC_std,'b','LineStyle','none')
 title('Mean Peak IPSC from BC onto BiC','FontSize',15,'FontWeight','bold')
 
-%% BC and BiC ipsc current onto BC gathered
+%% BC and BiC ipsc current onto BC, BiC and AAC gathered %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 % Sum all ipsc currents
-all_ipsc_on_BC = [];
+
+all_ipsc_on_BC_BiC = [];
+all_epsc_on_BC_BiC = [];
+all_ipsc_on_BC_BiC_AAC = [];
+all_ipsc_on_BC_AAC = [];
+all_epsc_on_BC_BiC_AAC = [];
+all_epsc_on_BC_AAC = [];
+
 for i = 1:1:14
     tot_cur_ipsc_on_BC =  current_BiC_on_BC(:,i) + current_BC_on_BC(:,i);
-    all_ipsc_on_BC = [all_ipsc_on_BC tot_cur_ipsc_on_BC];
+    tot_cur_ipsc_on_BiC =  current_BiC_on_BiC(:,i) + current_BC_on_BiC(:,i);
+    tot_cur_ipsc_on_AAC =  current_BiC_on_AAC(:,i) + current_BC_on_AAC(:,i);
+    
+    tot_cur_ipsc_on_BC_BiC = tot_cur_ipsc_on_BC + tot_cur_ipsc_on_BiC;
+    tot_cur_ipsc_on_BC_BiC_AAC = tot_cur_ipsc_on_AAC + tot_cur_ipsc_on_AAC;
+    tot_cur_ipsc_on_BC_AAC = tot_cur_ipsc_on_AAC + tot_cur_ipsc_on_AAC;
+    
+    all_ipsc_on_BC_BiC = [all_ipsc_on_BC_BiC tot_cur_ipsc_on_BC_BiC];
+    all_ipsc_on_BC_BiC_AAC = [all_ipsc_on_BC_BiC_AAC tot_cur_ipsc_on_BC_BiC_AAC];
+    all_ipsc_on_BC_AAC = [all_ipsc_on_BC_AAC tot_cur_ipsc_on_BC_AAC];
+    
+    % EPSC
+    tot_cur_epsc_on_BC_BiC_AAC =  current_PYR_on_BC(:,i) + current_PYR_on_BiC(:,i) + current_PYR_on_AAC(:,i);
+    all_epsc_on_BC_BiC_AAC = [all_epsc_on_BC_BiC_AAC tot_cur_epsc_on_BC_BiC_AAC];
+    
+    tot_cur_epsc_on_BC_AAC =  current_PYR_on_BC(:,i) + current_PYR_on_AAC(:,i);
+    all_epsc_on_BC_AAC = [all_epsc_on_BC_AAC tot_cur_epsc_on_BC_AAC];
 end
 
-% Find the peaks of the summed ipsc currents
-peaks_all_PV_on_BC = [];
+%% Find the peaks of the summed ipsc currents %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+peaks_all_PV_on_BC_BiC = [];
 for k = 1:1:14
-    [pks, locs] = findpeaks(all_ipsc_on_BC(:,k),'MinPeakDistance',4000); % peak detection
-    temp_cur = all_ipsc_on_BC(:,k);
+    [pks, locs] = findpeaks(all_ipsc_on_BC_BiC(:,k),'MinPeakDistance',4000); % peak detection
+    temp_cur = all_ipsc_on_BC_BiC(:,k);
     allrows = (1:40000)';
     notpeak = setdiff(allrows,locs);
     for t = 1:1:numel(notpeak)
@@ -655,38 +791,252 @@ for k = 1:1:14
         temp_cur(element,:) = 0;
     end
     peaks_all = temp_cur;
-    peaks_all_PV_on_BC = [peaks_all_PV_on_BC peaks_all];
+    peaks_all_PV_on_BC_BiC = [peaks_all_PV_on_BC_BiC peaks_all];
 end
 
-%% BC and BiC ipsc currents together onto BC - graph and table
 
-IPSC_all_on_BC = [];
-ipsc_all_on_BC = [];
+peaks_all_PV_on_BC_AAC = [];
+for k = 1:1:14
+    [pks, locs] = findpeaks(all_ipsc_on_BC_AAC(:,k),'MinPeakDistance',4000); % peak detection
+    temp_cur = all_ipsc_on_BC_BiC(:,k);
+    allrows = (1:40000)';
+    notpeak = setdiff(allrows,locs);
+    for t = 1:1:numel(notpeak)
+        element = notpeak(t,:);
+        temp_cur(element,:) = 0;
+    end
+    peaks_all = temp_cur;
+    peaks_all_PV_on_BC_AAC = [peaks_all_PV_on_BC_AAC peaks_all];
+end
+
+% Find the peaks of the summed ipsc currents
+peaks_all_PV_on_BC_BiC_AAC = [];
+for k = 1:1:14
+    [pks, locs] = findpeaks(all_ipsc_on_BC_BiC_AAC(:,k),'MinPeakDistance',4000); % peak detection
+    temp_cur = all_ipsc_on_BC_BiC_AAC(:,k);
+    allrows = (1:40000)';
+    notpeak = setdiff(allrows,locs);
+    for t = 1:1:numel(notpeak)
+        element = notpeak(t,:);
+        temp_cur(element,:) = 0;
+    end
+    peaks_all = temp_cur;
+    peaks_all_PV_on_BC_BiC_AAC = [peaks_all_PV_on_BC_BiC_AAC peaks_all];
+end
+
+peaks_PYR_on_BC_BiC_AAC = [];
+for k = 1:1:14
+    [pks, locs] = findpeaks(all_epsc_on_BC_BiC_AAC(:,k),'MinPeakDistance',4000); % peak detection
+    temp_cur = all_epsc_on_BC_BiC_AAC(:,k);
+    allrows = (1:40000)';
+    notpeak = setdiff(allrows,locs);
+    for t = 1:1:numel(notpeak)
+        element = notpeak(t,:);
+        temp_cur(element,:) = 0;
+    end
+    peaks_all = temp_cur;
+    peaks_PYR_on_BC_BiC_AAC = [peaks_PYR_on_BC_BiC_AAC peaks_all];
+end
+
+peaks_PYR_on_BC_AAC = [];
+for k = 1:1:14
+    [pks, locs] = findpeaks(all_epsc_on_BC_AAC(:,k),'MinPeakDistance',4000); % peak detection
+    temp_cur = all_epsc_on_BC_AAC(:,k);
+    allrows = (1:40000)';
+    notpeak = setdiff(allrows,locs);
+    for t = 1:1:numel(notpeak)
+        element = notpeak(t,:);
+        temp_cur(element,:) = 0;
+    end
+    peaks_all = temp_cur;
+    peaks_PYR_on_BC_AAC = [peaks_PYR_on_BC_AAC peaks_all];
+end
+
+%% BC and BiC ipsc currents together onto BC, BiC - graph and table %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+IPSC_all_on_BC_BiC = [];
+ipsc_all_on_BC_BiC = [];
 for i = 1:1:14 % number of PYR cells
-    pks_ipsc_all = peaks_all_PV_on_BC(:,i);
+    pks_ipsc_all = peaks_all_PV_on_BC_BiC(:,i);
     pks_ipsc_all(pks_ipsc_all == 0) = [];
     ipsc_all_mean = mean(pks_ipsc_all);
     ipsc_all_std = std(pks_ipsc_all);
-    ipsc_all_on_BC = [ipsc_all_mean;ipsc_all_std];
-    IPSC_all_on_BC = [IPSC_all_on_BC ipsc_all_on_BC];
+    ipsc_all_on_BC_BiC = [ipsc_all_mean;ipsc_all_std];
+    IPSC_all_on_BC_BiC = [IPSC_all_on_BC_BiC ipsc_all_on_BC_BiC];
 end 
 
-IPSC_all_on_BC = array2table(IPSC_all_on_BC);
-IPSC_all_on_BC.Properties.VariableNames = {'BC1'...
-    'BC2' 'BC3' 'BC4' 'BC5' 'BC6'...
-    'BC7' 'BC8' 'BC9' 'BC10' 'BC11'...
-    'BC12' 'BC13' 'BC14' 'BC15'};
+IPSC_all_on_BC_BiC = array2table(IPSC_all_on_BC_BiC);
+IPSC_all_on_BC_BiC.Properties.VariableNames = {'BC_BiC1'...
+    'BC_BiC2' 'BC_BiC3' 'BC_BiC4' 'BC_BiC5' 'BC_BiC6'...
+    'BC_BiC7' 'BC_BiC8' 'BC_BiC9' 'BC_BiC10' 'BC_BiC11'...
+    'BC_BiC12' 'BC_BiC13' 'BC_BiC14'};
 
-IPSC_all_on_BC_mean = table2array(IPSC_all_on_BC(1,:));
-IPSC_all_on_BC_std = table2array(IPSC_all_on_BC(2,:));
-x = linspace(0,14,length(IPSC_all_on_BC_mean));
+IPSC_all_on_BC_BiC_mean = table2array(IPSC_all_on_BC_BiC(1,:));
+IPSC_all_on_BC_BiC_std = table2array(IPSC_all_on_BC_BiC(2,:));
+x = linspace(0,14,length(IPSC_all_on_BC_BiC_mean));
 figure
-scatter(x,IPSC_all_on_BC_mean,'black','filled');
-xlabel('Individual Basket Cells','FontSize',13,'FontWeight','bold');
+scatter(x,IPSC_all_on_BC_BiC_mean,'black','filled');
+xlabel('Each Point Includes BC, BiC ','FontSize',13,'FontWeight','bold');
 ylabel('Mean Peak IPSC','FontSize',13,'FontWeight','bold');
 hold on;
-errorbar(x,IPSC_all_on_BC_mean,IPSC_all_on_BC_std,'b','LineStyle','none')
-title('Mean Peak IPSC from BC and BiC onto BC','FontSize',15,'FontWeight','bold')
+errorbar(x,IPSC_all_on_BC_BiC_mean,IPSC_all_on_BC_BiC_std,'b','LineStyle','none')
+title('Mean Peak IPSC from BC, BiC onto BC, BiC','FontSize',15,'FontWeight','bold')
+
+%% BC and BiC ipsc currents together onto BC, BiC and AAC - graph and table %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
+IPSC_all_on_BC_BiC_AAC = [];
+ipsc_all_on_BC_BiC_AAC = [];
+for i = 1:1:14 % number of PYR cells
+    pks_ipsc_all = peaks_all_PV_on_BC_BiC_AAC(:,i);
+    pks_ipsc_all(pks_ipsc_all == 0) = [];
+    ipsc_all_mean = mean(pks_ipsc_all);
+    ipsc_all_std = std(pks_ipsc_all);
+    ipsc_all_on_BC_BiC_AAC = [ipsc_all_mean;ipsc_all_std];
+    IPSC_all_on_BC_BiC_AAC = [IPSC_all_on_BC_BiC_AAC ipsc_all_on_BC_BiC_AAC];
+end 
+
+IPSC_all_on_BC_BiC_AAC = array2table(IPSC_all_on_BC_BiC_AAC);
+IPSC_all_on_BC_BiC_AAC.Properties.VariableNames = {'BC_BiC_AAC1'...
+    'BC_BiC_AAC2' 'BC_BiC_AAC3' 'BC_BiC_AAC4' 'BC_BiC_AAC5' 'BC_BiC_AAC6'...
+    'BC_BiC_AAC7' 'BC_BiC_AAC8' 'BC_BiC_AAC9' 'BC_BiC_AAC10' 'BC_BiC_AAC11'...
+    'BC_BiC_AAC12' 'BC_BiC_AAC13' 'BC_BiC_AAC14'};
+
+IPSC_all_on_BC_BiC_AAC_mean = table2array(IPSC_all_on_BC_BiC_AAC(1,:));
+IPSC_all_on_BC_BiC_AAC_std = table2array(IPSC_all_on_BC_BiC_AAC(2,:));
+x = linspace(0,14,length(IPSC_all_on_BC_BiC_AAC_mean));
+figure
+scatter(x,IPSC_all_on_BC_BiC_AAC_mean,'black','filled');
+xlabel('Each Point Includes BC, BiC, AAC','FontSize',13,'FontWeight','bold');
+ylabel('Mean Peak IPSC','FontSize',13,'FontWeight','bold');
+hold on;
+errorbar(x,IPSC_all_on_BC_BiC_AAC_mean,IPSC_all_on_BC_BiC_AAC_std,'b','LineStyle','none')
+title('Mean Peak IPSC from BC, BiC, AAC and BC, BiC, AAC','FontSize',15,'FontWeight','bold')
+
+%% BC and BiC ipsc currents together onto BC, AAC - graph and table %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+ 
+IPSC_all_on_BC_AAC = [];
+ipsc_all_on_BC_AAC = [];
+for i = 1:1:14 % number of PYR cells
+    pks_ipsc_all = peaks_all_PV_on_BC_AAC(:,i);
+    pks_ipsc_all(pks_ipsc_all == 0) = [];
+    ipsc_all_mean = mean(pks_ipsc_all);
+    ipsc_all_std = std(pks_ipsc_all);
+    ipsc_all_on_BC_AAC = [ipsc_all_mean;ipsc_all_std];
+    IPSC_all_on_BC_AAC = [IPSC_all_on_BC_AAC ipsc_all_on_BC_AAC];
+end 
+
+IPSC_all_on_BC_AAC = array2table(IPSC_all_on_BC_AAC);
+IPSC_all_on_BC_AAC.Properties.VariableNames = {'BC_AAC1'...
+    'BC_AAC2' 'BC_AAC3' 'BC_AAC4' 'BC_AAC5' 'BC_AAC6'...
+    'BC_AAC7' 'BC_AAC8' 'BC_AAC9' 'BC_AAC10' 'BC_AAC11'...
+    'BC_AAC12' 'BC_AAC13' 'BC_AAC14'};
+
+IPSC_all_on_BC_AAC_mean = table2array(IPSC_all_on_BC_AAC(1,:));
+IPSC_all_on_BC_AAC_std = table2array(IPSC_all_on_BC_AAC(2,:));
+x = linspace(0,14,length(IPSC_all_on_BC_AAC_mean));
+figure
+scatter(x,IPSC_all_on_BC_AAC_mean,'black','filled');
+xlabel('Each Point Includes BC, AAC','FontSize',13,'FontWeight','bold');
+ylabel('Mean Peak IPSC','FontSize',13,'FontWeight','bold');
+hold on;
+errorbar(x,IPSC_all_on_BC_AAC_mean,IPSC_all_on_BC_AAC_std,'b','LineStyle','none')
+title('Mean Peak IPSC from BC, AAC onto BC, AAC','FontSize',15,'FontWeight','bold')
+
+%% EPSC currents together onto BC, AAC - graph and table %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+EPSC_on_BC_AAC = [];
+epsc_on_BC_AAC = [];
+for i = 1:1:14 % number of PYR cells
+    pks_epsc_all = peaks_PYR_on_BC_AAC(:,i);
+    pks_epsc_all(pks_epsc_all == 0) = [];
+    epsc_all_mean = mean(pks_epsc_all);
+    epsc_all_std = std(pks_epsc_all);
+    epsc_on_BC_AAC = [epsc_all_mean;ipsc_all_std];
+    EPSC_on_BC_AAC = [EPSC_on_BC_AAC epsc_on_BC_AAC];
+end 
+
+EPSC_on_BC_AAC = array2table(EPSC_on_BC_AAC);
+EPSC_on_BC_AAC.Properties.VariableNames = {'BC_AAC1'...
+    'BC_AAC2' 'BC_AAC3' 'BC_AAC4' 'BC_AAC5' 'BC_AAC6'...
+    'BC_AAC7' 'BC_AAC8' 'BC_AAC9' 'BC_AAC10' 'BC_AAC11'...
+    'BC_AAC12' 'BC_AAC13' 'BC_AAC14'};
+
+EPSC_on_BC_AAC_mean = table2array(EPSC_on_BC_AAC(1,:));
+EPSC_on_BC_AAC_std = table2array(EPSC_on_BC_AAC(2,:));
+x = linspace(0,14,length(EPSC_on_BC_AAC_mean));
+figure
+scatter(x,EPSC_on_BC_AAC_mean,'black','filled');
+xlabel('Each Point Includes BC, AAC','FontSize',13,'FontWeight','bold');
+ylabel('Mean Peak EPSC','FontSize',13,'FontWeight','bold');
+hold on;
+errorbar(x,EPSC_on_BC_AAC_mean,EPSC_on_BC_AAC_std,'b','LineStyle','none')
+title('Mean Peak EPSC onto BC, BiC','FontSize',15,'FontWeight','bold')
+
+%% EPSC currents together onto BC, BiC, AAC - graph and table %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+EPSC_on_BC_BiC_AAC = [];
+epsc_on_BC_BiC_AAC = [];
+for i = 1:1:14 % number of PYR cells
+    pks_epsc_all = peaks_PYR_on_BC_BiC_AAC(:,i);
+    pks_epsc_all(pks_epsc_all == 0) = [];
+    epsc_all_mean = mean(pks_epsc_all);
+    epsc_all_std = std(pks_epsc_all);
+    epsc_on_BC_BiC_AAC = [epsc_all_mean;ipsc_all_std];
+    EPSC_on_BC_BiC_AAC = [EPSC_on_BC_BiC_AAC epsc_on_BC_BiC_AAC];
+end 
+
+EPSC_on_BC_BiC_AAC = array2table(EPSC_on_BC_BiC_AAC);
+EPSC_on_BC_BiC_AAC.Properties.VariableNames = {'BC_BiC_AAC1'...
+    'BC_BiC_AAC2' 'BC_BiC_AAC3' 'BC_BiC_AAC4' 'BC_BiC_AAC5' 'BC_BiC_AAC6'...
+    'BC_BiC_AAC7' 'BC_BiC_AAC8' 'BC_BiC_AAC9' 'BC_BiC_AAC10' 'BC_BiC_AAC11'...
+    'BC_BiC_AAC12' 'BC_BiC_AAC13' 'BC_BiC_AAC14'};
+
+EPSC_on_BC_BiC_AAC_mean = table2array(EPSC_on_BC_BiC_AAC(1,:));
+EPSC_on_BC_BiC_AAC_std = table2array(EPSC_on_BC_BiC_AAC(2,:));
+x = linspace(0,14,length(EPSC_on_BC_AAC_mean));
+figure
+scatter(x,EPSC_on_BC_BiC_AAC_mean,'black','filled');
+xlabel('Each Point Includes BC, BiC, AAC','FontSize',13,'FontWeight','bold');
+ylabel('Mean Peak EPSC','FontSize',13,'FontWeight','bold');
+hold on;
+errorbar(x,EPSC_on_BC_BiC_AAC_mean,EPSC_on_BC_BiC_AAC_std,'b','LineStyle','none')
+title('Mean Peak EPSC onto BC, BiC, AAC','FontSize',15,'FontWeight','bold')
+
+%% E/I Ratios on BC, BiC Cells %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+IPSC_BC_BiC_AAC_on_BC_BiC_AAC = table2array(IPSC_all_on_BC_BiC_AAC);
+IPSC_BC_AAC_on_BC_AAC = table2array(IPSC_all_on_BC_AAC);
+
+EPSC_on_BC_AAC = table2array(EPSC_on_BC_AAC);
+EPSC_on_BC_BiC_AAC = table2array(EPSC_on_BC_BiC_AAC);
+
+%IPSC_BC_on_BC = table2array(IPSC_BC_on_BC);
+%IPSC_all_on_BC= table2array(IPSC_all_on_BC);  % all refers to BC and BiC together
+%EPSC_BC = table2array(EPSC_BC);
+%%
+Ratios_BC_AAC = [];
+Ratios_BC_BiC_AAC = [];
+E_I_BC_AAC = abs(EPSC_on_BC_AAC(1,:)./IPSC_BC_AAC_on_BC_AAC(1,:))';
+E_I_BC_BiC_AAC = abs(EPSC_on_BC_BiC_AAC(1,:)./IPSC_BC_BiC_AAC_on_BC_BiC_AAC(1,:))';
+
+cells = 1:14;
+Ratios_BC_AAC = [Ratios_BC_AAC cells' E_I_BC_AAC];
+Ratios_BC_AAC = array2table(Ratios_BC_AAC);
+Ratios_BC_AAC.Properties.VariableNames = {'cell_number' 'Ratio_BC_AAC'};
+
+Ratios_BC_BiC_AAC = [Ratios_BC_BiC_AAC cells' E_I_BC_BiC_AAC];
+Ratios_BC_BiC_AAC = array2table(Ratios_BC_BiC_AAC);
+Ratios_BC_BiC_AAC.Properties.VariableNames = {'cell_number' 'Ratio_BC_BiC_AAC'};
+
+%%
+uitable('Data',Ratios_BC_AAC{:,:},'ColumnName',Ratios_BC_AAC.Properties.VariableNames,...
+    'Units', 'Normalized', 'Position',[0, 0, 1, 1]);
+
+%%
+uitable('Data',Ratios_BC_BiC_AAC{:,:},'ColumnName',Ratios_BC_BiC_AAC.Properties.VariableNames,...
+    'Units', 'Normalized', 'Position',[0, 0, 1, 1]);
 
 
 %% E/I Ratios on PYR Cells
